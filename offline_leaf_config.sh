6a02cc37e398036e6c2038aa68316440e0d534dd
MacIverLab <maciver@northwestern.edu>
2024-03-23 07:20:07 -0600

    Add files via upload

---
+++ offline_leaf_config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+GIT_PATH="/Users/maciver/Downloads/offline_leaf_test/65e9bfc9957f46e743210a77/"
+
+
+WATCH_PATH_OVERLEAF="$GIT_PATH"
+
+
+#######
+# No edits below this point should be needed
+#######################################
+
+COMMIT_INTERVAL_SECONDS=15 # Minimum gap between commits
+GIT_PULL_INTERVAL_SECONDS=20 # Interval to perform git pull in background
+DEBOUNCE_SECONDS=15 # After an fswatch event is detected, wait this long before detecting another
+
+
+# Locations of fswatch and convert
+FSWATCH="/opt/homebrew/bin/fswatch"
+CONVERT="/opt/homebrew/bin/convert"
+
+# Temp files and paths
+FSWATCH_OUTPUT_FILE_OVERLEAF=$(mktemp /tmp/offline_leaf.XXXXXXXX)
+
